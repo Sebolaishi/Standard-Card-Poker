6a7ed8697d5f6a5599fa09229628ece7a1d115da
lodwinmoloto <lodwin@anylytical.co.za>
2021-01-24 23:17:23 +0200

    Card hands listed in a decreasing rank order.

---
+++ src/main/java/com/standard/card/utilities/CardRank.java
@@ -0,0 +1,17 @@
+package com.standard.card.utilities;
+
+/**
+ * Card Rank enums object identifies a list of cards in their respective ranks.
+ */
+public enum CardRank {
+    STRAIGHT_FLUSH,
+    FOUR_OF_A_QUEEN,
+    FULL_HOUSE,
+    FLUSH,
+    STRAIGHT,
+    THREE_OF_A_KIND,
+    TWO_PAIR,
+    ONE_PAIR,
+    HIGH_CARDS
+
+}
