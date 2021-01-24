145572503c4d93c38331a98de5513d341b680164
lodwinmoloto <lodwin@anylytical.co.za>
2021-01-24 22:42:38 +0200

    Setup project execution point

---
+++ src/main/java/com/standard/card/main/StandardCardPoker.java
@@ -0,0 +1,15 @@
+package com.standard.card.main;
+
+/**
+ * Five Card Poker Draw main class
+ */
+public class StandardCardPoker {
+
+    /**
+     * Application entry point starts here.
+     * @param args
+     */
+    public static void main(String[] args){
+
+    }
+}
