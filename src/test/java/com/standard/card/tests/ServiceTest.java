ac5d33de4e6ad0fcb6edae0caee80987f74b32f6
lodwinmoloto <lodwin@anylytical.co.za>
2021-01-25 00:14:35 +0200

    Code refactor

--- src/test/java/com/standard/card/tests/ServiceTest.java
+++ src/test/java/com/standard/card/tests/ServiceTest.java
@@ -2,18 +2,31 @@ package com.standard.card.tests;
 
 import com.standard.card.services.CardsDeck;
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
-
-import java.util.List;
 
 public class ServiceTest {
 
+    /**
+     * Properties
+     */
     private static final int DECK_SIZE = 52;
 
+    /**
+     * Initialize dependencies on test execution
+     */
+    @BeforeClass
+    public static void setUpBeforeClass()
+    {
+        CardsDeck.getDeck(DECK_SIZE);
+    }
+
+    /**
+     * Testing deck shuffling capability.
+     */
     @Test
     public void getDeckTest(){
-        List<Integer> deck = CardsDeck.getDeck(DECK_SIZE);
-        System.out.println(deck);
-        Assert.assertNotNull(deck);
+        System.out.println("\n" + CardsDeck.getDeck());
+        Assert.assertNotNull("List is not null ", CardsDeck.getDeck());
     }
 }
